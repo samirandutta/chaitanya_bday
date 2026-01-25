14c39db00cb71a3a468781a47917dc7e738260b9
Samiran Dutta <142884934+samirandutta@users.noreply.github.com>
2026-01-25 11:16:02 +0100

    Update gate.js

--- assets/js/gate.js
+++ assets/js/gate.js
@@ -12,12 +12,10 @@ const noBtn = document.getElementById("noBtn");
 question.textContent = `Are you ${PERSON}?`;
 
 yesBtn.addEventListener("click", () => {
-  // go to cake page
-  window.location.href = "cake.html";
+  window.location.href = "login.html";
 });
 
 noBtn.addEventListener("click", () => {
-  // show second image + message, hide buttons
   catImg.src = "assets/img/cat2.jpg";
   message.textContent = "This website is not for you.";
   btnRow.style.display = "none";
