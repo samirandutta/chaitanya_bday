a17d93124815f9e462b3305067cbce6c65721d5a
Samiran Dutta <142884934+samirandutta@users.noreply.github.com>
2026-02-08 13:52:08 +0100

    Update letter2.js

--- assets/js/letter2.js
+++ assets/js/letter2.js
@@ -1,23 +1,11 @@
-// assets/js/letter2.js
 (function () {
   if (sessionStorage.getItem("authed") !== "1") {
     window.location.href = "index.html";
     return;
   }
 
-  const env = document.getElementById("envelope");
-
-  function go(){
-    // If main.html tries to autoplay your song, this gives it a better chance
+  document.getElementById("toMain").addEventListener("click", () => {
     sessionStorage.setItem("autoplaySong", "1");
     window.location.href = "main.html";
-  }
-
-  env.addEventListener("click", go);
-  env.addEventListener("keydown", (e) => {
-    if (e.key === "Enter" || e.key === " ") {
-      e.preventDefault();
-      go();
-    }
   });
 })();
