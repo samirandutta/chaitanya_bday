852a4413c9e5bfd06393b30534a4ab7c93ad5ea9
Samiran Dutta <142884934+samirandutta@users.noreply.github.com>
2026-02-08 12:55:54 +0100

    Create letter1.js

---
+++ assets/js/letter1.js
@@ -0,0 +1,10 @@
+// assets/js/letter1.js
+(function () {
+  if (sessionStorage.getItem("authed") !== "1") {
+    window.location.href = "index.html";
+    return;
+  }
+  document.getElementById("toLetter2").addEventListener("click", () => {
+    window.location.href = "letter2.html";
+  });
+})();
